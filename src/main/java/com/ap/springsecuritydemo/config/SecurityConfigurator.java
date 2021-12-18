ce50ae3bcd343699e94e1fd77ea75c1cc8bd0687
aparnabhure <aparnabhure1983@gmail.com>
2021-12-18 21:43:01 +0530

    Authentication with embedded H2 database with default schema

--- src/main/java/com/ap/springsecuritydemo/config/SecurityConfigurator.java
+++ src/main/java/com/ap/springsecuritydemo/config/SecurityConfigurator.java
@@ -1,25 +1,45 @@
 package com.ap.springsecuritydemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SecurityConfigurator extends WebSecurityConfigurerAdapter {
+    @Autowired
+    DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //In memory authentication
-        auth.inMemoryAuthentication()
-                .withUser("aparna").password("aparna").roles("admin")
-                //Add multiple users
-                .and()
-                .withUser("test").password("test").roles("user");
+        //JDBC authentication with H2 embedded database, which creates database
+        auth.jdbcAuthentication()
+                //This makes connectivity with H2
+                .dataSource(dataSource)
+                // This creates default tables like User and Authentication
+                .withDefaultSchema()
+                .withUser(User.withUsername("aparna").password("aparna").roles("admin"))
+                .withUser(User.withUsername("test").password("test").roles("user"));
 
     }
+
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        //In memory authentication
+//        auth.inMemoryAuthentication()
+//                .withUser("aparna").password("aparna").roles("admin")
+//                //Add multiple users
+//                .and()
+//                .withUser("test").password("test").roles("user");
+//
+//    }
+
+
 
     //Authorization at API level with User Role
     @Override
